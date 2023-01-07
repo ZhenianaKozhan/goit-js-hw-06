d17ec2e92c8120c95790c44b738c47ad5669ea65
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-07 13:27:51 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  nameColor: document.querySelector('.color'),
+  changeColorBtn: document.querySelector('.change-color')
+}
+
+refs.changeColorBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.nameColor.textContent = getRandomHexColor();
+}
