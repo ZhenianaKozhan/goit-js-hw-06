556a406af0a60e60e6de27ca285d8de6b6ed5506
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-07 22:56:36 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Error. All fields must be filled')
+    };
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
