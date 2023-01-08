9e47c79a09dd732794d5646549e5ebc8725758df
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-08 20:25:00 +0200

    homework done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divControls = document.getElementById('controls');
+const divBoxes = document.getElementById('boxes')
+const refs = {
+  input: divControls.firstElementChild,
+  btnCreate: divControls.children[1],
+  btnDestroy: divControls.children[2]
+}
+
+refs.btnCreate.addEventListener('click', createBoxes)
+refs.btnDestroy.addEventListener('click', destroyBoxes)
+
+function createBoxes(event) {
+  createBoxess(refs.input.value)
+}
+
+function createBoxess(amount) {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div')
+    div.style.height = `${30 + 10 * i}px`
+    div.style.width = `${30 + 10 * i}px`
+    div.style.backgroundColor = getRandomHexColor()
+    divBoxes.append(div)
+  }
+}
+
+function destroyBoxes() {
+  divBoxes.innerHTML = ''
+}
